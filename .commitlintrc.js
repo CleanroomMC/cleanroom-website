fb506bbc2b7515c7710c7d5f936f913cfb4d016c
baka-gourd <36119339+baka-gourd@users.noreply.github.com>
2024-02-23 19:01:59 +0800

    feat: add commitlint

---
+++ .commitlintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  prompt: {
+    scopes: [
+      { value: "zh, wiki", name: "Wiki(zh)" },
+      { value: "en, wiki", name: "Wiki(en)" },
+      { value: "components", name: "Custom components" },
+    ],
+  },
+};
