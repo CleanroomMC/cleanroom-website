56ba0db0cc54f6230e3e094ad606eab092846c70
baka-gourd <36119339+baka-gourd@users.noreply.github.com>
2024-03-01 09:30:40 +0800

    feat: add new scope

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     scopes: [
       { value: "zh, wiki", name: "Wiki(zh)" },
       { value: "en, wiki", name: "Wiki(en)" },
+      { value: "site", name: "Site" },
       { value: "components", name: "Custom components" },
     ],
   },
